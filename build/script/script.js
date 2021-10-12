f6a796e6b16c21b7842b4cc7a6d324c74e02ad93
YumiChann <jufredenucci@gmx.fr>
2021-10-12 14:00:58 +0200

    Maj4Session

--- build/script/script.js
+++ build/script/script.js
@@ -4,3 +4,5 @@ ScrollReveal().reveal('#about', { delay: 750 });
 
 ScrollReveal().reveal('#front',{delay: 500});
 ScrollReveal().reveal('#end', { delay: 1000 });
+ScrollReveal().reveal('#frame',{delay: 1500});
+ScrollReveal().reveal('#maquette', { delay: 2000 });
